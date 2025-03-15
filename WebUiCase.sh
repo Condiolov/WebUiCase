769bef199621b723a609afae3ca6913f20b3815d
thiago <thiagocondiolov@hotmail.com>
2025-03-15 10:08:13 -0300

    Modificações:
    1. Alteração na busca de cabeçalho: Muda de buscar "Host:" para "Origin:".
    2. Verificação e tratamento da string `REQUISICAO` para redirecionar requisição '/' para '/WebUiCase.html'.
    3. Condição adicional verifica se o arquivo solicitado termina em `.html`.
    4. Após verificar se o arquivo existe, imprime uma mensagem de erro caso não exista.
    5. Comentários adicionais e formatações de strings foram ajustados para melhor clareza.

---
+++ WebUiCase.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# echo $0
+PORT=3000
+
+#echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+
+WebUiCase() {
+
+    REQUEST_HEADER=""
+    while IFS= read -r line; do
+        # Adiciona cada linha ao conteúdo do cabeçalho
+        REQUEST_HEADER+="$line"$'\n'
+        # Verifica se a linha está vazia (fim do cabeçalho HTTP)
+        if [[ "$line" == $'\r' || -z "$line" ]]; then
+            break
+        fi
+    done
+    # echo $REQUEST_HEADER
+
+    CONTENT_LENGTH=$(echo "$REQUEST_HEADER" | grep -i 'Content-Length:' | awk '{print $2}')
+    CONTENT_LENGTH=${CONTENT_LENGTH//$'\r'/} # Remove o \r (retorno de carro)
+    #     CONTENT_HOST=$(echo "$REQUEST_HEADER" | grep -i 'Host:' | awk '{print $2}')
+    CONTENT_HOST=$(echo "$REQUEST_HEADER" | grep -i 'Origin:' | awk '{print $2}')
+
+    #     echo $REQUEST_HEADER >file.txt
+    CONTENT_HOST=${CONTENT_HOST//$'\r'/} # Remove o \r (retorno de carro)
+    REQUISICAO=$(echo "$REQUEST_HEADER" | grep -i 'GET' | awk '{print $2}')
+
+    #     echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+    [[ "$REQUISICAO" == '/' ]] && REQUISICAO='/WebUiCase.html'
+
+    #     echo $REQUISICAO
+
+    [[ "$REQUISICAO" =~ \.html ]] &&
+        {
+            echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n"
+            [[ -f "$PWD$REQUISICAO" ]] && cat "$PWD$REQUISICAO" || echo "⚠️ Pagina não existe!! "
+        } && exit
+
+}
+
+[[ -n "$1" ]] && $1 && exit
+
+pgrep -f "ncat -l -p $PORT" >/dev/null 2>&1 && exit
+brave-browser --profile-directory=Default --app-id=hbblfifohofgngfbjbiimbbcimepbdcb
+while true; do
+    ncat -l -p $PORT -e "$0 WebUiCase"
+done
